2579d4df184e17a3422aac216048d6a8f8254227
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2025-12-01 12:19:48 -0500

    day 01 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,23 +1,51 @@
 fn main() {
     let input = include_str!("../../inputs/day01.txt");
-    let result = solve(input);
+    let result = solve_part_one(input);
     println!("Part one: {result}");
+    let result = solve_part_two(input, 50);
+    println!("Part two: {result}");
 }
 
-fn solve(input: &str) -> usize {
+fn solve_part_one(input: &str) -> usize {
     let mut position: isize = 50;
     let mut count = 0;
 
     for line in input.lines() {
         let (dir, value) = line.split_at(1);
         let value = value.parse::<isize>().unwrap();
-        if dir == "R" {
-            position += value;
-        } else {
-            position -= value;
-        }
+        let value = if dir == "R" { value } else { -value };
+        position += value;
         position = position % 100;
         if position == 0 {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn solve_part_two(input: &str, pos: isize) -> usize {
+    let mut position: isize = pos;
+    let mut count = 0;
+
+    for line in input.lines() {
+        let (dir, value) = line.split_at(1);
+        let value = value.parse::<isize>().unwrap();
+        let quotient: usize = (value / 100).try_into().unwrap();
+        count += quotient;
+        let change = value % 100;
+        let change = if dir == "R" { change } else { -change };
+
+        if position != 0 && (position + change < 0 || position + change > 100) {
+            count += 1;
+        }
+
+        position += change;
+
+        position = position % 100;
+        if position < 0 {
+            position = 100 + position;
+        }
+        if position == 0 && change != 0 {
             count += 1;
         }
     }
@@ -40,6 +68,57 @@ L1
 L99
 R14
 L82";
-        assert_eq!(solve(input), 3);
+        assert_eq!(solve_part_one(input), 3);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let position = 50;
+        let input = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+        assert_eq!(solve_part_two(input, position), 6);
+    }
+
+    #[test]
+    fn test_part2_example_subset_one() {
+        let position = 50;
+        let input = "L68
+L30
+R48
+L5
+R60";
+        assert_eq!(solve_part_two(input, position), 3);
+    }
+
+    #[test]
+    fn test_quotients_case_one() {
+        let position = 1;
+        let input = "L1
+L1000";
+
+        assert_eq!(solve_part_two(input, position), 11);
+    }
+
+    #[test]
+    fn test_quotients_case_two() {
+        let position = 1;
+        let input = "R199";
+        assert_eq!(solve_part_two(input, position), 2);
+    }
+
+    #[test]
+    fn test_quotients_case_three() {
+        let position = 0;
+        let input = "L1000";
+
+        assert_eq!(solve_part_two(input, position), 10);
     }
 }
