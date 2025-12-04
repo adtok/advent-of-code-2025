9726ecced77e1d757b51c4643b19d1118d9a45a6
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2025-12-04 17:00:12 -0500

    day 04 part 2 simplification?

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,18 +1,24 @@
+type FactoryFloor = Vec<Vec<usize>>;
+
 fn main() {
     let input = include_str!("../../inputs/day04.txt");
     let result = solve_part_one(input);
     println!("Part one: {result}");
-    let result = 0;
+    let result = solve_part_two(input);
     println!("Part two: {result}");
 }
 
-fn parse_input(input: &str) -> Vec<Vec<usize>> {
+fn parse_input(input: &str) -> FactoryFloor {
     let width = input.find('\n').unwrap();
-    let padded: Vec<Vec<char>> = [".".repeat(width), input.to_string(), ".".repeat(width)]
-        .join("\n")
-        .lines()
-        .map(|line| format!(".{line}.").chars().collect())
-        .collect();
+    let padded: Vec<Vec<char>> = [
+        ".".repeat(width),
+        input.trim().to_string(),
+        ".".repeat(width),
+    ]
+    .join("\n")
+    .lines()
+    .map(|line| format!(".{line}.").chars().collect())
+    .collect();
     padded
         .iter()
         .enumerate()
@@ -26,10 +32,9 @@ fn parse_input(input: &str) -> Vec<Vec<usize>> {
                     if padded[y][x] == '.' {
                         0
                     } else {
-                        neighbors(x, y)
-                            .iter()
-                            .filter(|p| padded[p.1][p.0] == '@')
-                            .count()
+                        let ns = neighbors(x, y);
+                        let result = ns.iter().filter(|p| padded[p.1][p.0] == '@').count();
+                        result
                     }
                 })
                 .collect()
@@ -53,27 +58,53 @@ fn neighbors(x: usize, y: usize) -> [(usize, usize); 9] {
 
 fn solve_part_one(input: &str) -> usize {
     let layout = parse_input(input);
-
-    let mut result = 0;
-
-    let result = layout
+    layout
         .iter()
-        .map(|row| row.iter().filter(|&count| (1..5).contains(count)).count())
-        .sum();
-
-    result
+        .map(|row| row.iter().filter(|&ns| (1..5).contains(ns)).count())
+        .sum()
 }
 
-fn remove_rolls(layout: &mut Vec<Vec<usize>>) -> (Vec<Vec<usize>>, usize) {
-    return (layout.clone(), 0);
+fn remove_rolls(layout: &FactoryFloor) -> (FactoryFloor, usize) {
+    let mut new_layout = layout.clone();
+    let height = new_layout.len();
+    let width = new_layout[0].len();
+    let mut removed = 0;
+    for y in 1..height - 1 {
+        for x in 1..width - 1 {
+            if new_layout[y][x] < 5 && new_layout[y][x] != 0 {
+                new_layout[y][x] = 0;
+                removed += 1;
+            }
+        }
+    }
+    for y in 1..height - 1 {
+        for x in 1..width - 1 {
+            if new_layout[y][x] != 0 {
+                new_layout[y][x] = neighbors(x, y)
+                    .iter()
+                    .filter(|p| new_layout[p.1][p.0] != 0)
+                    .count();
+            }
+        }
+    }
+
+    return (new_layout, removed);
 }
 
 fn solve_part_two(input: &str) -> usize {
-    let lines = parse_input(input);
-    let width = lines[0].len();
-    let height = lines.len();
+    let mut layout = parse_input(input);
 
-    43
+    let mut result = 0;
+    loop {
+        let (new_layout, n_removed) = remove_rolls(&layout);
+        result += n_removed;
+        if n_removed == 0 {
+            break;
+        }
+        layout = new_layout;
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -109,7 +140,7 @@ mod tests {
 @.@@@.@@@@
 .@@@@@@@@.
 @.@.@@@.@.";
-        let result = 43;
+        let result = solve_part_two(input);
 
         assert_eq!(result, 43);
     }
