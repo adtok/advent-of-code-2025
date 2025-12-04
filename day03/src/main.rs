14ab5706584a7b6b24fe018b01feaeace420a0e3
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2025-12-04 12:19:35 -0500

    day 03 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     let input = include_str!("../../inputs/day03.txt");
     let result = solve_part_one(input);
     println!("Part one: {result}");
+    let result = solve_part_two(input, 12);
+    println!("Part two: {result}");
 }
 
 fn solve_part_one(input: &str) -> u32 {
@@ -29,6 +31,32 @@ fn solve_part_one(input: &str) -> u32 {
     return result;
 }
 
+fn n_largest(nums: Vec<u32>, n_digits: usize) -> usize {
+    let len = nums.len();
+    let mut result: usize = 0;
+    let mut idx = 0;
+    for n in (0..n_digits).rev() {
+        let mut max = u32::MIN;
+        let start = if result == 0 { idx } else { idx + 1 };
+        for i in start..len - (n) {
+            if nums[i] > max {
+                max = nums[i];
+                idx = i;
+            }
+        }
+        result = result * 10 + max as usize;
+    }
+    return result;
+}
+
+fn solve_part_two(input: &str, n_digits: usize) -> usize {
+    input
+        .lines()
+        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .map(|nums| n_largest(nums, n_digits))
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -42,5 +70,27 @@ mod tests {
         let result = solve_part_one(input);
 
         assert_eq!(result, 357);
+    }
+
+    #[test]
+    fn test_solve_part_one_n_largest() {
+        let input = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+        let result = solve_part_two(input, 2);
+
+        assert_eq!(result, 357);
+    }
+
+    #[test]
+    fn test_solve_part_two_sample() {
+        let input = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+        let result = solve_part_two(input, 12);
+
+        assert_eq!(result, 3121910778619);
     }
 }
