000f5c9b6338866d46798f77fa462ceb969a83c8
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2025-12-05 14:03:48 -0500

    day 05 part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,7 +1,11 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = include_str!("../../inputs/day05.txt");
     let result = solve_part_one(input);
     println!("Part one: {result}");
+    let result = solve_part_two(input);
+    println!("Part two: {result}");
 }
 
 fn solve_part_one(input: &str) -> usize {
@@ -24,13 +28,45 @@ fn solve_part_one(input: &str) -> usize {
         .count()
 }
 
+fn max(a: usize, b: usize) -> usize {
+    if a > b { a } else { b }
+}
+
+fn overlap(a: (usize, usize), b: (usize, usize)) -> Option<(usize, usize)> {
+    match (a, b) {
+        (a, b) if (a.0..=a.1).contains(&b.0) => Some((a.0, max(a.1, b.1))),
+        (a, b) if (b.0..=b.1).contains(&a.0) => Some((b.0, max(a.1, b.1))),
+        _ => None,
+    }
+}
+
+fn solve_part_two(input: &str) -> usize {
+    let mut ranges: Vec<(usize, usize)> = vec![];
+    for line in input.split_once("\n\n").unwrap().0.lines() {
+        let (start, end) = line.split_once("-").unwrap();
+        let mut range: (usize, usize) = (start.parse().unwrap(), end.parse().unwrap());
+        loop {
+            if let Some(idx) = ranges
+                .iter()
+                .position(|&other| overlap(range, other).is_some())
+            {
+                let overlapped = ranges[idx];
+                range = overlap(range, overlapped).unwrap();
+                ranges.remove(idx);
+            } else {
+                ranges.push(range);
+                break;
+            }
+        }
+    }
+    ranges.iter().map(|range| (range.0..=range.1).count()).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_solve_part_one_sample() {
-        let input = "3-5
+    const SAMPLE_INPUT: &str = "3-5
 10-14
 16-20
 12-18
@@ -41,7 +77,64 @@ mod tests {
 11
 17
 32";
-        let result = solve_part_one(input);
+
+    #[test]
+    fn test_solve_part_one_sample() {
+        let result = solve_part_one(SAMPLE_INPUT);
         assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn test_solve_part_two_sample() {
+        let result = solve_part_two(SAMPLE_INPUT);
+        assert_eq!(result, 14);
+    }
+
+    #[test]
+    fn test_overlap_a_below_b() {
+        let a = (3, 5);
+        let b = (10, 14);
+
+        assert_eq!(overlap(a, b), None);
+    }
+
+    #[test]
+    fn test_overlap_b_below_a() {
+        let a = (10, 14);
+        let b = (3, 5);
+
+        assert_eq!(overlap(a, b), None);
+    }
+
+    #[test]
+    fn test_overlap_a_extends_b() {
+        let a = (10, 14);
+        let b = (12, 18);
+
+        assert_eq!(overlap(a, b), Some((10, 18)));
+    }
+
+    #[test]
+    fn test_overlap_b_extends_a() {
+        let b = (10, 14);
+        let a = (12, 18);
+
+        assert_eq!(overlap(a, b), Some((10, 18)));
+    }
+
+    #[test]
+    fn test_overlap_a_contains_b() {
+        let a = (2, 6);
+        let b = (3, 5);
+
+        assert_eq!(overlap(a, b), Some((2, 6)));
+    }
+
+    #[test]
+    fn test_overlap_b_contains_a() {
+        let a = (3, 5);
+        let b = (2, 6);
+
+        assert_eq!(overlap(a, b), Some((2, 6)));
     }
 }
